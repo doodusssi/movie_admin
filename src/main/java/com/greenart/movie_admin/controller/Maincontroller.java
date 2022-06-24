7c96edbed096c542245c99529d777e56b4520c8f
doodussi <rladuden03@naver.com>
2022-06-24 11:51:46 +0900

    프로젝트 파일 최초 업로드

---
+++ src/main/java/com/greenart/movie_admin/controller/Maincontroller.java
@@ -0,0 +1,12 @@
+package com.greenart.movie_admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Maincontroller {
+    @GetMapping("/")
+    public String getMain() {
+        return "/index";
+    }
+}
