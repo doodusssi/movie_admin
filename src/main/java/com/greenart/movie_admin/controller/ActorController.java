33f01cdbfb33a859a4af31171224fdb3b5df3c4b
doodussi <rladuden03@naver.com>
2022-06-28 17:58:53 +0900

    actor 추가 완료 / movie info 작업 진행도 70%

---
+++ src/main/java/com/greenart/movie_admin/controller/ActorController.java
@@ -0,0 +1,43 @@
+package com.greenart.movie_admin.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.greenart.movie_admin.data.actor.ActorCountryVO;
+import com.greenart.movie_admin.data.actor.CinemaActorListVO;
+import com.greenart.movie_admin.mapper.ActorMapper;
+import com.greenart.movie_admin.service.ActorService;
+
+@Controller
+@RequestMapping("/actor")
+public class ActorController {
+    @Autowired ActorService actor_service;
+    @GetMapping("/list")
+    public String getActorList(
+    @RequestParam @Nullable String keyword,
+    @RequestParam @Nullable Integer page,
+    @RequestParam @Nullable String country, Model model) {
+        
+        
+        if(page == null) page = 1;
+        
+        model.addAttribute("countryURL", actor_service.makeActorCountryLink(keyword));
+        model.addAttribute("pagerURL", actor_service.makePagerURL(keyword, country));
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("page", page);
+        model.addAttribute("country", country);
+        model.addAttribute("list", actor_service.getCinemaActorList(keyword, country, page));
+
+        return "/actor/list";
+    
+    }
+}
--- src/main/webapp/WEB-INF/views/movie/genre.jsp
+++ src/main/webapp/WEB-INF/views/movie/genre.jsp
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="/assets/js/common/util.js"></script>
+    
     <script src="/assets/js/movie/genre.js"></script>
     <link rel="stylesheet" href="/assets/css/movie/genre.css">
 </head>
