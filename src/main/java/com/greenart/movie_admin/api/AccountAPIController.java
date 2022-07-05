2aa27e3ea6d8297969019077073fc0057639eb9a
doodussi <rladuden03@naver.com>
2022-07-05 16:56:58 +0900

    1차 시안 완료

--- src/main/java/com/greenart/movie_admin/api/AccountAPIController.java
+++ src/main/java/com/greenart/movie_admin/api/AccountAPIController.java
@@ -1,7 +1,13 @@
 package com.greenart.movie_admin.api;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
@@ -11,20 +17,26 @@ import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.greenart.movie_admin.data.account.AdminAccountInfoVO;
+import com.greenart.movie_admin.data.history.AdminAccessHistoryVO;
 import com.greenart.movie_admin.mapper.AccountMapper;
+import com.greenart.movie_admin.mapper.HistoryMapper;
 import com.greenart.movie_admin.utils.AESAlgorithm;
 
 @RestController
 @RequestMapping("/api/account")
 public class AccountAPIController {
     @Autowired AccountMapper account_mapper;
+    @Autowired HistoryMapper history_mapper;
     @PutMapping ("/add")
     public ResponseEntity< Map<String, Object>>addAccountInfo(@RequestBody AdminAccountInfoVO data)throws Exception{
         Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
@@ -86,13 +98,13 @@ public class AccountAPIController {
         Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
         
         account_mapper.deleteAdminAccount(seq);
-
+        
         resultMap.put("status", true);
         resultMap.put("message", "관리자 정보를 삭제했습니다.");
-
+        
         return resultMap;
     }
-
+    
     @GetMapping("/list")
     public Map<String, Object> getAccountList(@RequestParam @Nullable String keyword, @RequestParam Integer page) {
         Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
@@ -109,5 +121,62 @@ public class AccountAPIController {
     @GetMapping("/select_one")
     public AdminAccountInfoVO selectAccountOne(@RequestParam Integer seq) {
         return account_mapper.selectAdminBySeq(seq);
+    }
+    @PostMapping("/login")
+    public Map<String, Object> postAdminLogin(@RequestBody AdminAccountInfoVO data, HttpSession session)throws Exception{
+        Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
+
+        data.setAai_pwd(AESAlgorithm.Encrypt(data.getAai_pwd()));
+        AdminAccountInfoVO user = account_mapper.loginUser(data);
+        
+        if(user == null) {
+            resultMap.put("status", false);
+            resultMap.put("message", "아이디 혹은 비밀번호 오류입니다.");
+        }
+        else{
+            String ip = null;
+            HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+            // ip = request.getRemoteAddr();
+            ip = request.getHeader("X-Forwarded-For");
+        
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getHeader("Proxy-Client-IP"); 
+        } 
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getHeader("WL-Proxy-Client-IP"); 
+        } 
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getHeader("HTTP_CLIENT_IP"); 
+        } 
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR"); 
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getHeader("X-Real-IP"); 
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getHeader("X-RealIP"); 
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getHeader("REMOTE_ADDR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { 
+            ip = request.getRemoteAddr(); 
+        }
+
+            resultMap.put("status", true);
+
+            // resultMap.put("ip", ip);
+            resultMap.put("message", "로그인 시간 : "+LocalDate.now()+" "+LocalTime.now());
+            session.setAttribute("loginUser", user);
+
+            AdminAccessHistoryVO history = new AdminAccessHistoryVO();
+            history.setAah_aai_seq(user.getAai_seq());
+            history.setAah_ip(ip);
+            history.setAah_start(new Date());
+            history.setAah_url("/login");
+            history_mapper.insertAdminHistory(history);
+        }
+        return resultMap;
     }
 }
